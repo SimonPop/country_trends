b5cb16699e7a8e6ba91119f9d60cc34b2e877a03
Simon Popelier <simon.popelier@gmail.com>
2023-03-07 23:41:58 +0100

    Change  in datasset & training

--- src/training.py
+++ src/training.py
@@ -1,5 +1,5 @@
 import torch
-from dataset import Dataset
+from whale_dataset import WhaleDataset
 from torch_geometric_temporal.nn import MTGNN
 from torch.utils.data import DataLoader
 import torch.optim as optim
@@ -9,7 +9,7 @@ from pathlib import Path
 
 if __name__ == "__main__":
 
-    dataset = Dataset(CONFIG.model_config.seq_length - 1, countries=CONFIG.countries)
+    dataset = WhaleDataset(CONFIG.model_config.seq_length - 1)
     loader = DataLoader(dataset, batch_size=8)
 
     model_config = CONFIG.model_config
@@ -18,13 +18,13 @@ if __name__ == "__main__":
     model = MTGNN(
             **model_config.dict()
         )
-
+    
     criterion = torch.nn.L1Loss()
     optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=CONFIG.momentum)
     running_loss = 0
 
     for epoch in range(CONFIG.epochs):
-        for i, (x, y) in tqdm(enumerate(loader)):
+        for i, (x, y) in enumerate(loader):
             optimizer.zero_grad()
             outputs = model(x)
 
@@ -33,9 +33,8 @@ if __name__ == "__main__":
             optimizer.step()
 
             running_loss += loss.item()
-            if i % 2000 == 1999:    # print every 2000 mini-batches
-                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
-                running_loss = 0.0
+        print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss/len(loader):.3f}')
+        running_loss = 0.0
 
     PATH = Path(__file__).parent.parent / "artifacts" / "model.pt"
     torch.save(model.state_dict(), PATH)
